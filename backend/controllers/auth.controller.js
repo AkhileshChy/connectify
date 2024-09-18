4d274134986156dda898a92b181aa96475d5ede9
Akhilesh <choudharyakhilesh2003@gmail.com>
2024-09-18 23:15:58 +0530

    login page done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -41,3 +41,28 @@ export const signup = async (req, res) => {
 		res.status(500).json({ message: "Internal server error" });
     }
 }
+
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user || !password){
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+        await res.cookie("jwt-connectify", token, {
+			httpOnly: true, 
+			maxAge: 3 * 24 * 60 * 60 * 1000,
+			sameSite: "strict", 
+			secure: process.env.NODE_ENV === "production", 
+		});
+        res.json({ message: "Logged in successfully" });
+    } catch (error) {
+        console.error("Error in login controller:", error);
+		res.status(500).json({ message: "Server error" });
+    }
+}
