254d72b85d58ddf9e31da759d41e01794571779a
Akhilesh <choudharyakhilesh2003@gmail.com>
2024-12-09 15:17:11 +0530

    project ready to dockerize and deploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -14,14 +16,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
-app.use(express.json());
+
+if (process.env.NODE_ENV !== "production") {
+	app.use(
+		cors({
+			origin: "http://localhost:5173",
+			credentials: true,
+		})
+	);
+}
+app.use(express.json({ limit: "5mb" })); 
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
@@ -29,6 +35,14 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { axiosInstance } from "../lib/axios";
+
+import ProfileHeader from "../components/ProfileHeader";
+import AboutSection from "../components/AboutSection";
+import ExperienceSection from "../components/ExperienceSection";
+import EducationSection from "../components/EducationSection";
+import SkillsSection from "../components/SkillsSection";
+import toast from "react-hot-toast";
 
 const ProfilePage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const { username } = useParams();
+	const queryClient = useQueryClient();
 
-export default ProfilePage
+	const { data: authUser, isLoading } = useQuery({
+		queryKey: ["authUser"],
+	});
+
+	const { data: userProfile, isLoading: isUserProfileLoading } = useQuery({
+		queryKey: ["userProfile", username],
+		queryFn: () => axiosInstance.get(`/users/${username}`),
+	});
+
+	const { mutate: updateProfile } = useMutation({
+		mutationFn: async (updatedData) => {
+			await axiosInstance.put("/users/profile", updatedData);
+		},
+		onSuccess: () => {
+			toast.success("Profile updated successfully");
+			queryClient.invalidateQueries(["userProfile", username]);
+		},
+	});
+
+	if (isLoading || isUserProfileLoading) return null;
+
+	const isOwnProfile = authUser.username === userProfile.data.username;
+	const userData = isOwnProfile ? authUser : userProfile.data;
+
+	const handleSave = (updatedData) => {
+		updateProfile(updatedData);
+	};
+
+	return (
+		<div className='max-w-4xl mx-auto p-4'>
+			<ProfileHeader userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
+			<AboutSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
+			<ExperienceSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
+			<EducationSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
+			<SkillsSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
+		</div>
+	);
+};
+export default ProfilePage;
